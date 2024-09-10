83e79c327b45965c15abd472affbcb8c94d02263
Filip Zagórski <f.zagorski1909@gmail.com>
2024-09-10 10:26:08 +0200

    Initial commit

---
+++ prisma/migrations/20240910075724_init/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE `Participant` (
+    `participant_id` VARCHAR(191) NOT NULL,
+    `event_id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `is_organizer` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`participant_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Event` (
+    `event_id` VARCHAR(191) NOT NULL,
+    `event_name` VARCHAR(191) NOT NULL,
+    `voting_start` DATETIME(3) NOT NULL,
+    `voting_end` DATETIME(3) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`event_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TerminStatus` (
+    `termin_status_id` VARCHAR(191) NOT NULL,
+    `day` DATETIME(3) NOT NULL,
+    `event_id` VARCHAR(191) NOT NULL,
+    `participant_id` VARCHAR(191) NOT NULL,
+    `status` ENUM('AVAILABLE', 'NOT_AVAILABLE') NOT NULL,
+
+    PRIMARY KEY (`termin_status_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Participant` ADD CONSTRAINT `Participant_event_id_fkey` FOREIGN KEY (`event_id`) REFERENCES `Event`(`event_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `TerminStatus` ADD CONSTRAINT `TerminStatus_event_id_fkey` FOREIGN KEY (`event_id`) REFERENCES `Event`(`event_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `TerminStatus` ADD CONSTRAINT `TerminStatus_participant_id_fkey` FOREIGN KEY (`participant_id`) REFERENCES `Participant`(`participant_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Event, PrismaClient } from '@prisma/client';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prismaService: PrismaService) {}
+  async getHello(): Promise<Event[]> {
+    return await this.prismaService.event.findMany();
   }
 }
